9ee93f72908379793d483194c2eadb50025eac28
ChloD2-0 <cdougherty1121@icloud.com>
2026-02-12 21:13:53 +0000

    P Task 2

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-flashcards = [ 
-     {
-     "Term": "
-     "Meaning": 
-    "Difficulty":
- }
-ashcards = [ 
-    {Term = input("Enter term: ")},
-    {Meaning = input("Meaning:  ")},
-    {Level = input("Difficulty level: ")},
-]
-flashcard.cards()
-def flashcard(x, y): 
-    question = input("Create new flashcard?🤔 (y/n)")
-    if question == "y":
-        x = input("Enter term: ")
-        # terms.append()
-        y = input("Enter definition: ")
-        # mean
+# flashcards = [ 
+#     {Term = input("Enter term: ")},
+#     {Meaning = input("Meaning:  ")},
+#     {Level = input("Difficulty level: ")},
+# ]
+# flashcard.cards
 
-flashcard
+# def flashcard(x, y): 
+#     question = input("Create new flashcard?🤔 (y/n)")
+#     if question == "y":
+#         x = input("Enter term: ")
+#         y = input("Enter definition: ")
+
+
+# flashcard(x,y)
+flashcards = 
+    {
+    "Term":,
+    "Meaning":,
+    "Level" :
+}
+
+def create_card():
+    input("Create new flashcard?🤔 (y/n)")
+    if == "y":
+        input("Enter term: ")
+        input("Meaning:  ")
+        input("Difficulty level: ")
+
+create_card()
+
+   
+
