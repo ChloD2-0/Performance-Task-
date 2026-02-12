f0a201ae8b8bb9d729705b28b428076029100b82
ChloD2-0 <cdougherty1121@icloud.com>
2026-02-12 20:34:29 +0000

    P Task

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+flashcards = [ 
+     {
+     "Term": "
+     "Meaning": 
+    "Difficulty":
+ }
+ashcards = [ 
+    {Term = input("Enter term: ")},
+    {Meaning = input("Meaning:  ")},
+    {Level = input("Difficulty level: ")},
+]
+flashcard.cards()
+def flashcard(x, y): 
+    question = input("Create new flashcard?🤔 (y/n)")
+    if question == "y":
+        x = input("Enter term: ")
+        # terms.append()
+        y = input("Enter definition: ")
+        # mean
 
+flashcard
