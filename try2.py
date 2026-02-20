434b0ee090725153919e7386ea0ea9a46cf9337d
ChloD2-0 <cdougherty1121@icloud.com>
2026-02-20 18:50:50 +0000

    PT

--- try2.py
+++ try2.py
@@ -1,6 +1,6 @@
-
-def menu():
-    options = input("Main Menu:\n 1. Create New Flashcard\n 2. Study\n 3. Delete Flashcard\nChoose an option: ")
+list_functions = []
+def menu(): 
+    options = input("Main Menu:\n 1. Create New Flashcard\n 2. Study\n 3. Delete Flashcard\nChoose a number: ")
    
     while options:
         if options == "1":
@@ -20,22 +20,31 @@ def menu():
                         flashcard_dict[question] = answer
                         print("New flashcard created")
                         return flashcard_dict
-            create_cards()
+            list_functions.append(create_cards())
             break
+        
         elif options == "2":
-            def study(dictionary):
-                flashcards = dictionary 
-                length = len(flashcards)
-                cards_missed = {}
-                correct = 0
-                for card in flashcards:
-                    print(card)
-                    input("")
-                    print(flashcards[card])
+             for i,(question, answer) in enumerate
+            # def study(dictionary):
+            #     flashcards = dictionary 
+            #     length = len(flashcards)
+            #     cards_missed = {}
+            #     correct = 0
+            #     for card in flashcards:
+            #         print(card)
+            #         input("")
+            #         print(flashcards[card])  
 
         elif options == "3":
-            return options
-     
-menu()
+            # return options
+            # answer = input("Do you want to see the menu again (yes or no): ")
+            # if answer == "yes":
+            #     list_functions[1]
+            
+            
 
 
+def want_another():
+            answer = input("Do you want to see the menu again (yes or no): ")
+            if answer == "yes":
+                list_functions[1]
